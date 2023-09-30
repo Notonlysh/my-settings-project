8a6e98e3e8760a29521ff93bfcfd58d1a78281e6
Notonalysh <sden0327@uni.sydney.edu.au>
2023-09-30 16:53:52 +1000

    git

---
+++ src/components/AppHomePage.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>Some content</div>
+  <div>Some other content</div>
+</template>
+
+<script>
+export default {
+  name: "AppHomePage.vue"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/AppSettings.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <h1>Settings</h1>
+
+    <label>
+      Username:
+      <input v-model="username" />
+    </label>
+
+    <label>
+      Background Color:
+      <select v-model="backgroundColor">
+        <option value="white">White</option>
+        <option value="blue">Blue</option>
+        <option value="red">Red</option>
+        <option value="green">Green</option>
+      </select>
+    </label>
+
+    <button @click="applySettings">Apply</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      username: '',
+      backgroundColor: 'white'
+    };
+  },
+  methods: {
+    applySettings() {
+      this.$emit('update-settings', {
+        username: this.username,
+        backgroundColor: this.backgroundColor
+      });
+    }
+  }
+};
+</script>
+
+<style scoped>
+/* Add your styles here */
+</style>
